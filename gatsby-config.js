38613e74a8fee168166f75005bfd821ace8de03f
Mohammed <heroboy107@gmail.com>
2022-09-11 18:04:31 +0200

    Gatseby project

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,15 @@ module.exports = {
     title: `Workout-Hero`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }]
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    `gatsby-transformer-excel`,
+    `gatsby-plugin-sass`,
+  ],
 };
